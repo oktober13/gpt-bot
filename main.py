137faa244edb6f517e8081401c5139cf3868e5d3
oktober13 <Robertxp129@gmail.com>
2023-07-17 10:24:08 +0500

    Update and clear garbage :safety_vest:

--- main.py
+++ main.py
@@ -37,52 +37,6 @@ def handle_start(message):
         bot.send_photo(message.chat.id, photo, caption=welcome_message, parse_mode='Markdown')
 
 
-# Обработчик команды /users
-@bot.message_handler(commands=['users'])
-def handle_users(message):
-    # Проверка идентификатора пользователя
-    if message.from_user.id not in admin_ids:
-        bot.reply_to(message, "У вас нет доступа к этой команде.")
-        return
-
-    # Подключение к базе данных
-    conn = connect_database()
-    c = conn.cursor()
-
-    # Запрос на выборку всех пользователей
-    c.execute("SELECT * FROM users")
-    user_list = c.fetchall()
-
-    # Формирование сообщения со списком пользователей
-    user_info = "Список пользователей:\n"
-    for user in user_list:
-        user_info += f"@{user[1]}: {user[0]}\n"
-
-    # Отправка сообщения с списком пользователей
-    bot.reply_to(message, user_info)
-
-    # Закрытие соединения с базой данных
-    conn.close()
-
-
-# Обработчик команды /context
-@bot.message_handler(commands=['context'])
-def handle_context(message):
-    # Подключение к базе данных
-    conn = connect_database()
-    c = conn.cursor()
-
-    # Обнуление контекста диалога для пользователя
-    c.execute("UPDATE users SET context = NULL WHERE id=?", (message.from_user.id,))
-    conn.commit()
-
-    # Отправка сообщения о сбросе контекста
-    bot.reply_to(message, "Контекст диалога сброшен. Начните новую беседу.")
-
-    # Закрытие соединения с базой данных
-    conn.close()
-
-
 # Обработчик всех остальных сообщений
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
@@ -92,15 +46,6 @@ def handle_message(message):
     # Подключение к базе данных
     conn = connect_database()
     c = conn.cursor()
-
-    # Получение контекста диалога из базы данных
-    # c.execute("SELECT context FROM users WHERE id=?", (message.from_user.id,))
-    # previous_context = c.fetchone()
-
-    # if previous_context:
-    #     # Если контекст найден, использовать его для продолжения диалога
-    #     previous_question = previous_context[0]
-    #     question = f"{previous_question} {question}"
 
     # Подготовка запроса к ChatGPT
     chat_input = {
@@ -121,9 +66,6 @@ def handle_message(message):
 
     # Извлечение ответа от ChatGPT
     answer = response.choices[-1].message.content.strip()  # Получение последнего ответа
-
-    # Добавление сообщения о сбросе контекста в конец ответа
-    # answer += "\n\nДля сброса диалога введите команду /context"
 
     # Отправка ответа пользователю
     bot.reply_to(message, answer)
